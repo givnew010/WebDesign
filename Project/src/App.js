0de044be413b5ffa7130f28a4cc254e7235a53fd
Abdulrahman Alsadi <givnew010@gmail.com>
2023-11-05 16:11:33 +0300

    Create project and delete file not used

---
+++ Project/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          This Frist Page Befor Update.
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
